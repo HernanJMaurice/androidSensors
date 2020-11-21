778de0f61f6c4759e7d877b6fddd1f34ece74b12
MisterMouse <Hernan_maurice@hotmail.com>
2020-11-21 01:53:34 -0300

    Archivo Main Activity Camio
    Agrego la subscripcion al Topic de Firebase para notificaciones

--- Codigo/Android Project/app/src/main/java/com/mauricesoa/mauricesensores/MainActivity.java
+++ Codigo/Android Project/app/src/main/java/com/mauricesoa/mauricesensores/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mauricesoa.mauricesensores;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -11,6 +12,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("all")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Subscrito Correctamente";
+                        if (!task.isSuccessful()) {
+                            msg = "Error suscripcion";
+                        }
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
 
         setContentView(R.layout.battery_check);
 
